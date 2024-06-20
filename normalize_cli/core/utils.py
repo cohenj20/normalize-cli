df90d41afb323d23aa7cae649e27d96c8d72d733
cohenj20 <cohenj20@sacredheart.edu>
2024-06-20 10:38:39 -0400

    new utility that checks if database exists

--- normalize_cli/core/utils.py
+++ normalize_cli/core/utils.py
@@ -1,3 +1,14 @@
 import logging
+import pandas as pd
 
 logger = logging.getLogger(__name__)
+
+def check_database_exists(conn):
+    query = '''
+    IF DB_ID('dms') IS NOT NULL
+        --code mine :)
+        print 'db exists'
+    '''
+
+    return pd.read_sql(query, con=conn)
+
