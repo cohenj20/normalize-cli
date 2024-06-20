760d29a2507c34809573390b19d6974f1d3a3925
cohenj20 <cohenj20@sacredheart.edu>
2024-06-20 10:43:45 -0400

    add db argument and add check for db

--- normalize_cli/core/connection.py
+++ normalize_cli/core/connection.py
@@ -3,6 +3,7 @@ import logging
 from sqlalchemy import create_engine
 
 from normalize_cli.cli.outputs import report_invalid_db_type
+from normalize_cli.core.utils import check_database_exists
 
 
 logger = logging.getLogger(__name__)
@@ -13,20 +14,22 @@ class SQLConnection():
     def __init__(
             self,
             server: str,
+            db: str,
             db_type: str
 
         ) -> None:
         self.server = server
+        self.db = db
         self.db_type = db_type
 
     def construct_uri(self):
         if self.db_type not in VALID_DB_TYPES:
             raise SystemExit(report_invalid_db_type(self.db_type))
         else:
-            if self.db_type == 'sqlserver':
+            if self.db_type == 'sqlserver' & check_database_exists(self.conn):
                 logger.info('Constructing uri.')
                 logger.debug(f'db_type: {self.db_type}')
-                self.uri = f'mssql+pyodbc://@{self.server}.sacredheart.edu/SHUDW?driver=ODBC+Driver+13+for+SQL+Server'
+                self.uri = f'mssql+pyodbc://@{self.server}.sacredheart.edu/{self.db}?driver=ODBC+Driver+13+for+SQL+Server'
                 logger.info('Successfully constructed uri.')
                 logger.debug(f'constructed uri: {self.uri}')
 
