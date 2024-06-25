db4eca9e8f5cc30cb9df6595e5b092b993b9c7dc
cohenj20 <cohenj20@sacredheart.edu>
2024-06-25 16:37:24 -0400

    add new options: tool for handling different replication tool configurations and variables; db > source_db to prevent confusion;views option if replicating views as well as tables

--- normalize_cli/core/run.py
+++ normalize_cli/core/run.py
@@ -3,26 +3,32 @@ import click
 
 from normalize_cli.core.logger import configure_logger
 from normalize_cli.core.connection import SQLConnection
-from normalize_cli.core.utils import check_database_exists
 from normalize_cli.cli.outputs import init_cli
+from normalize_cli.core.models import Metadata
 
 configure_logger()
 
 logger = logging.getLogger(__name__)
 
 @click.command()
+@click.option('--tool', help='Replication platofrm being used. Options: [airbyte]')
 @click.option('--server', help='A sacredheart domain server name or IP address where the SHUDW database resides')
-@click.option('--db', help='Database being replicated.')
+@click.option('--source_db', help='Source database being replicated.')
+@click.option('--target_db', help='Target database to which data is being replicated.')
+@click.option('--target_schema', help='Target database to which data is being replicated.')
 @click.option('--db_type', help='Database type. Options: [sqlserver, oracle, mysql, postgresql]')
-def run(server, db, db_type):
+@click.option('--views', help='Boolean flag for inclusion of views in metadata retrieval. Options: [true, false]')
+def run(tool, server, source_db, target_db, target_schema, db_type, views):
     """
     Generates normalize dbt models.
     """
     init_cli()
-    engine = SQLConnection(server, db, db_type)
+    engine = SQLConnection(server, source_db, db_type)
     engine.construct_uri()
     engine.create_connection()
     engine.test_connection()
+    metadata = Metadata(engine.conn, views=views, target_db = target_db, target_schema=target_schema)
+    metadata.generate_models()
 
 
     
