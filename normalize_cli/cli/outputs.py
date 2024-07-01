fbd2c36a955902fb41c68fefa376437ebe111590
cohenj20 <cohenj20@sacredheart.edu>
2024-07-01 10:56:10 -0400

    new output for successful model generation

--- normalize_cli/cli/outputs.py
+++ normalize_cli/cli/outputs.py
@@ -1,5 +1,5 @@
 import click
-import colorama
+from colorama import Fore
 
 def init_cli():
     click.echo(f'======================================================================================================================')
@@ -10,3 +10,5 @@ def init_cli():
 def report_invalid_db_type(db_type):
     click.echo(f"The DB type you have entered ({db_type}) is invalid. Run 'normalize run -h' to see the list of valid db types")
 
+def report_successful_model_generation(model_count, cwd):
+    click.echo(Fore.GREEN + f"{model_count} models successfully generated in {cwd}.")
